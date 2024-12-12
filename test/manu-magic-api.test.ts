98d307d6c5140b64d47b7614edfd636389e9e0d2
Sho <o.shogo.1021q@gmail.com>
2024-12-13 08:19:56 +0900

    Initial commit

---
+++ test/manu-magic-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ManuMagicApi from '../lib/manu-magic-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/manu-magic-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ManuMagicApi.ManuMagicApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
